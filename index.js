27fd3601ebaaef0484e57f49849308afc2cb569e
afmdanielwin <danielfarias380@gmail.com>
2020-05-12 19:31:51 -0300

    Types of response

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Learning app listening on port 3000!')
+app.get('/', (req, res) => {
+    // res.send('<h1>Posso passar HTML</h1>')
+    
+    // res.json({
+    //     name: 'iPad',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
+    
+    res.json([
+        {id: 7, name: 'Ana', position: 1},
+        {id: 34, name: 'Bia', position: 2},
+        {id: 73, name: 'Carlos', position: 3}
+    ])
 })
 
-app.use((req, res) => {
-    res.send('use method')
+app.listen(3000, () => {
+    console.log('Learning app listening on port 3000!')
 })
